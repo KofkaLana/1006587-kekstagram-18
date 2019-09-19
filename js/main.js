541b72de07f0a42055abb3a62229b6b0465a3ecc
Svetlana <orrisa@yandex.ru>
2019-09-19 10:41:12 +0300

    Исправление ошибок eslint

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'],
-    DESCRIPTION = ['Отдыхали как могли!', 'Отдыхать - не работать)))', 'Новый опыт, новые ощущения', 'Это просто праздник!)', 'Приключения - наше все!!', 'Всё отлично!', 'Just VAU!!!', 'Отдых - он такой)'],
-    NAMES = ['Иван', 'Антон', 'Мария', 'Ксюша', 'Виктор', 'Юлия', 'Лолита', 'Вахтанг'];
+var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var DESCRIPTION = ['Отдыхали как могли!', 'Отдыхать - не работать)))', 'Новый опыт, новые ощущения', 'Это просто праздник!)', 'Приключения - наше все!!', 'Всё отлично!', 'Just VAU!!!', 'Отдых - он такой)'];
+var NAMES = ['Иван', 'Антон', 'Мария', 'Ксюша', 'Виктор', 'Юлия', 'Лолита', 'Вахтанг'];
 
 // генерация адресов картинок
 
-function generateArrayRandomNumber (min, max) {
-  var totalNumbers = max - min + 1,
-      arrayTotalNumbers = [],
-      arrayRandomNumbers = [],
-      tempRandomNumber;
+function generateArrayRandomNumber(min, max) {
+  var totalNumbers = max - min + 1;
+  var arrayTotalNumbers = [];
+  var arrayRandomNumbers = [];
+  var tempRandomNumber;
   while (totalNumbers--) {
     arrayTotalNumbers.push(totalNumbers + min);
   }
@@ -34,15 +34,16 @@ function getRandomInRange(min, max) {
 
 var getPhotoDescription = function (i) {
   var PhotoDescription = {
-      url: 'photos/' + N[i] + '.jpg',
-      description: DESCRIPTION[Math.floor((Math.random() * DESCRIPTION.length))],
-      likes: getRandomInRange(15, 200),
-      comments:  {
-        avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
-        message: COMMENTS[Math.floor((Math.random() * COMMENTS.length))],
-        name: NAMES[Math.floor((Math.random() * NAMES.length))]
-      }
-  }
+    url: 'photos/' + N[i] + '.jpg',
+    description: DESCRIPTION[Math.floor((Math.random() * DESCRIPTION.length))],
+    likes: getRandomInRange(15, 200),
+    comments: {
+      avatar: 'img/avatar-' + getRandomInRange(1, 6) + '.svg',
+      message: COMMENTS[Math.floor((Math.random() * COMMENTS.length))],
+      name: NAMES[Math.floor((Math.random() * NAMES.length))]
+    }
+  };
+
   return PhotoDescription;
 };
 
@@ -72,7 +73,7 @@ var renderPhoto = function (photo) {
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < N.length; i++) {
+for (i = 0; i < N.length; i++) {
   fragment.appendChild(renderPhoto(photos[i]));
 }
 photosList.appendChild(fragment);
