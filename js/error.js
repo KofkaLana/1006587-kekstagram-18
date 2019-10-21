754c54a3d28304f299a6a4574968bfb0457a295a
Svetlana <orrisa@yandex.ru>
2019-10-21 22:12:56 +0300

    недоисправляла

--- js/error.js
+++ js/error.js
@@ -7,8 +7,8 @@
 
   var error = function (message) {
     window.errorNode = errorTemplate.cloneNode(true);
-    errorNode.querySelector('.error__title').innerHTML = 'Ошибка соединения с сервером <br>' + message;
-    main.appendChild(errorNode);
+    window.errorNode.querySelector('.error__title').innerHTML = 'Ошибка соединения с сервером <br>' + message;
+    main.appendChild(window.errorNode);
   };
 
   window.error = {
